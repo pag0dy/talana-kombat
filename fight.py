af46f50819b072709135c5c4b2318c8364a134cf
pag0dy <paulina.godoydelc@gmail.com>
2022-07-31 18:57:01 -0400

    mejora el relato

--- fight.py
+++ fight.py
@@ -13,21 +13,37 @@ class Fight:
         print(f'El jugador 2 tiene los siguientes movimientos: {self.pl2_moves}')
 
  
-    def start_fight(self):
+    def prepare_fight(self):
+        #Decide quién va primero
         if len(self.pl1_moves['movimientos'][0] + self.pl1_moves['golpes'][0]) < len(self.pl2_moves['movimientos'][0] + self.pl2_moves['golpes'][0]):
             print('Comienza atacando el jugador 1')
-            # fight_line_up = {[self.pl1_moves, self.pl1],[self.pl2_moves, self.pl2]}
-            fight_line_up = [[self.pl1, list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes']))], [self.pl2, list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))]]
+            fighting_order = (self.pl1, self.pl2)
+            fight_line_up = [list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes'])), list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))]
         else:
-            print('Comienza el jugador 2')  
-            fight_line_up = [[self.pl2, list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))], [self.pl1, list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes']))]]
-        print(fight_line_up)
-        combinations = lambda x,y: x+y
-        combo_list = []
-        for i in fight_line_up[0][1]:
-            combo_list.append(combinations(i[0], i[1]))
-        print(combo_list)
-        fight_line_up[0][0].attack(combo_list)
+            print('Comienza el jugador 2') 
+            fighting_order = (self.pl2, self.pl1) 
+            fight_line_up = [[list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))], [list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes']))]]
+        print('*******')
+        return fighting_order, fight_line_up
 
-        # print(list(zip(fight_line_up[1]['movimientos'], fight_line_up[1]['golpes'])))
+    def fight(self, fighting_order, fight_line_up):
+        print('Fight!')
+        i = 0
+        player1 = fighting_order[0]
+        player2 = fighting_order[1]
+        while i in range(0,4):
+            move1 = fight_line_up[0][i]
+            move2 = fight_line_up[1][i]
+            player1.attack(move1)
+            player2.attack(move2)
+            i += 1
+        
+
+        # fight_narration = list(zip(fighting_order, fight_line_up))
+        # print(fight_narration(fighting_order, fight_line_up))
+        # return fight_narration
+        
+        
+
+
 
--- players.py
+++ players.py
@@ -5,25 +5,36 @@ class Player:
         self.name = n
         self.attacks = {'combinations': [], 'damages': [], 'names':[]}
         self.energy = 6
+        self.direction = ''
 
     def sayHi(self):
         print(f'Hola!, mi nombre es {self.name}.')
 
     def attack(self, combo_list):
-        loose_moves = {'W': 'arriba', 'S':'abajo', 'A':'izquierda', 'D':'derecha'}
+        loose_moves = {'W': 'hacia arriba', 'S':'hacia abajo', 'A':'a la izquierda', 'D':'a la derecha'}
         loose_attacks = {'P': 'puño', 'K':'patada'}
         combo_names = self.attacks['names']
-        
-        for combo in combo_list:
-            if combo in self.attacks['combinations']:
-                index = self.attacks['combinations'].index(combo)
-
-                print(f'{self.name} {combo_names[index]}')
-            else:
-                for char in combo:
-                    if char in loose_moves:
-                        print(f'se mueve {loose_moves[char]}')
-                    if char in loose_attacks:
-                        print(f'conecta un golpe de {loose_attacks[char]}')
+        combination = combo_list[0] + combo_list[1]
+        i = 0
+        this_move = f'{self.name}'
+        if combination in self.attacks['combinations']:
+            index = self.attacks['combinations'].index(combination)
+            this_move += f' conecta un {combo_names[index]}'
+        else:
+            while i in range(0, len(combination)):                       
+                char = list(combination).pop(i)
+                if char in loose_moves:
+                    if len(this_move) > len(f'{self.name}'):
+                        this_move += f', luego {loose_moves[char]}'
                     else:
-                        pass
+                        this_move += f' se mueve {loose_moves[char]}'
+                elif char in loose_attacks:
+                    if len(this_move) > len(f'{self.name}'):
+                        this_move += f' y conecta un golpe de {loose_attacks[char]}'
+                    else:
+                        this_move += f' conecta un golpe de {loose_attacks[char]}'
+                elif combination[i:len(combination)] in self.attacks['combinations']:
+                    index = self.attacks['combinations'].index(combination)
+                    this_move += f' conecta un {combo_names[index]}'      
+                i += 1
+        print(this_move)
