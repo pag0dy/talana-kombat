3d2b19017d56379c95bfc167ff18ac2a948bf39f
pag0dy <paulina.godoydelc@gmail.com>
2022-07-31 21:18:57 -0400

    afina detalles del relato y agrega comentarios a funciones

--- fight.py
+++ fight.py
@@ -1,45 +1,151 @@
-
+import time
 
 # Clase de pelea
+
 class Fight:
     def __init__(self, player1, player2, pelea):
+        '''Función constructora de la clase Peleas.
+           :params player1 y player2, los jugadores que participarán en este combate (instancias de la clase Player).
+           :params pelea, JSON con los movimientos y golpes de cada jugador.
+        '''
         self.pl1_moves = pelea["player1"]
         self.pl2_moves = pelea["player2"]
         self.pl1 = player1
         self.pl2 = player2
+        self.counter = 0
 
-    def resumen_pelea(self):
-        print(f'El jugador 1 tiene los siguientes movimientos: {self.pl1_moves}')
-        print(f'El jugador 2 tiene los siguientes movimientos: {self.pl2_moves}')
-
- 
     def prepare_fight(self):
-        #Decide quién va primero
-        if len(self.pl1_moves['movimientos'][0] + self.pl1_moves['golpes'][0]) < len(self.pl2_moves['movimientos'][0] + self.pl2_moves['golpes'][0]):
+        '''Función que decide quién parte atacando y retorna los parámetros necesarios para comenzar la pelea.
+            :retorna fighting_order, lista con los jugadores en el órden en el que atacarán.
+            :retorna fighting_line_up, lista con los movimientos y golpes de cada jugador, en el mismo órden de la lista anterior.
+            '''
+        time.sleep(1)
+        print('Bienvenidos a...')
+        time.sleep(1)
+        print('--------------------------------------------------')
+        print('▀█▀ ▄▀█ █░░ ▄▀█ █▄░█ ▄▀█ █▄▀ █▀█ █▀▄▀█ █▄▄ ▄▀█ ▀█▀')
+        print('░█░ █▀█ █▄▄ █▀█ █░▀█ █▀█ █░█ █▄█ █░▀░█ █▄█ █▀█ ░█░')
+        print('--------------------------------------------------')
+
+        # Se evalúa si esta es la primera pelea entre los jugadores o si es un desempate.
+        if self.counter == 0:
+            if len(self.pl1_moves['movimientos'][0] + self.pl1_moves['golpes'][0]) < len(self.pl2_moves['movimientos'][0] + self.pl2_moves['golpes'][0]):
+                time.sleep(1)
+                print('------------------------------')
+                print('Comienza atacando el jugador 1')
+                print('------------------------------')
+                fighting_order = (self.pl1, self.pl2)
+                fight_line_up = [list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes'])), list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))]
+            else:
+                time.sleep(1)
+                print('------------------------------')
+                print('Comienza atacando el jugador 2') 
+                print('------------------------------')
+                fighting_order = (self.pl2, self.pl1) 
+                fight_line_up = [list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes'])), list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes']))]
+
+        elif self.counter == 1:
+            if len(self.pl1_moves['movimientos'][0]) < len(self.pl2_moves['movimientos'][0]):
+                time.sleep(1)
+                print('------------------------------')
+                print('Comienza atacando el jugador 1')
+                print('------------------------------')
+                fighting_order = (self.pl1, self.pl2)
+                fight_line_up = [list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes'])), list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))]
+            else:
+                time.sleep(1)
+                print('------------------------------')
+                print('Comienza atacando el jugador 2') 
+                print('------------------------------')
+                fighting_order = (self.pl2, self.pl1) 
+                fight_line_up = [list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes'])), list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes']))]
+
+        elif self.counter == 2:
+            if len(self.pl1_moves['golpes'][0]) < len(self.pl2_moves['golpes'][0]):
+                time.sleep(1)
+                print('------------------------------')
+                print('Comienza atacando el jugador 1')
+                print('------------------------------')
+                fighting_order = (self.pl1, self.pl2)
+                fight_line_up = [list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes'])), list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))]
+            else:
+                time.sleep(1)
+                print('------------------------------')
+                print('Comienza atacando el jugador 2') 
+                print('------------------------------')
+                fighting_order = (self.pl2, self.pl1) 
+                fight_line_up = [list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes'])), list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes']))]
+
+        else:
+            time.sleep(1)
+            print('------------------------------')
             print('Comienza atacando el jugador 1')
+            print('------------------------------')
             fighting_order = (self.pl1, self.pl2)
             fight_line_up = [list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes'])), list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes']))]
-        else:
-            print('Comienza el jugador 2') 
-            fighting_order = (self.pl2, self.pl1) 
-            fight_line_up = [list(zip(self.pl2_moves['movimientos'], self.pl2_moves['golpes'])), list(zip(self.pl1_moves['movimientos'], self.pl1_moves['golpes']))]
-        print('*******')
-        return fighting_order, fight_line_up
 
+        return fighting_order, fight_line_up
+        
     def fight(self, fighting_order, fight_line_up):
-        print('Fight!')
-        i = 0
+        ''' Función que ejecuta la pelea y muestra el resultado de esta.
+            :params fighting_order, tupla que entrega los jugadores en el órden en el que van a combatir.
+            :params fight_line_up, lista que combina los movimientos y golpes de cada jugador en el órden correspondiente.
+        '''
+        time.sleep(1)
+        print('-----------------------')
+        print('█▀█ █▀▀ ▄▀█ █▀▄ █▄█ ▀█ ▀█')
+        print('█▀▄ ██▄ █▀█ █▄▀ ░█░ ░▄ ░▄')
+        print('-----------------------')
+        time.sleep(1)
+        print('-----------------------')
+        print('█▀▀ █ █▀▀ █░█ ▀█▀ █ █ █')
+        print('█▀░ █ █▄█ █▀█ ░█░ ▄ ▄ ▄')
+        print('-----------------------')
+        # Se asignan los jugadores según el órden en el que atacarán
         player1 = fighting_order[0]
         player2 = fighting_order[1]
-        while i in range(0,len(fight_line_up[1])):
+        # Se aseegura de que ambos jugadores comiencen con la energía completa
+        player1.reset_energy()
+        player2.reset_energy()
+        i = 0
+        fight_turns = len(fight_line_up[0]) if len(fight_line_up[0]) > len(fight_line_up[1]) else len(fight_line_up[1])
+        while i in range(0,fight_turns):
             move1 = fight_line_up[0][i] if fight_line_up[0][i] else None
             move2 = fight_line_up[1][i] if fight_line_up[1][i] else None
+            time.sleep(1)
             damage1 = player1.attack(move1)
-            player2.update_energy(damage1) 
-            print(f'energía {player2.name} = {player2.energy} ')   
+            player2.update_energy(damage1)  
+            time.sleep(1)
             damage2 = player2.attack(move2)
             player1.update_energy(damage2)
-            print(f'energía {player1.name} = {player1.energy} ')  
+            if player1.energy <= 0 and player2.energy > 0:
+                print(f'{player2.name} es el ganador!')
+                time.sleep(1)
+                print('*********************************************************')
+                print('█▀▀ █▀█ █▄░█ █▀▀ █▀█ ▄▀█ ▀█▀ █▀   █▀▀ █░█ ▄▀█ █▀▄▀█ █▀█ █')
+                print('█▄▄ █▄█ █░▀█ █▄█ █▀▄ █▀█ ░█░ ▄█   █▄▄ █▀█ █▀█ █░▀░█ █▀▀ ▄')
+                print('*********************************************************')
+                time.sleep(2)
+                break
+            elif player2.energy <= 0 and player1.energy > 0:
+                time.sleep(1)
+                print(f'{player1.name} es el ganador!')
+                print('*********************************************************')
+                print('█▀▀ █▀█ █▄░█ █▀▀ █▀█ ▄▀█ ▀█▀ █▀   █▀▀ █░█ ▄▀█ █▀▄▀█ █▀█ █')
+                print('█▄▄ █▄█ █░▀█ █▄█ █▀▄ █▀█ ░█░ ▄█   █▄▄ █▀█ █▀█ █░▀░█ █▀▀ ▄')
+                print('*********************************************************')
+                time.sleep(2)
+                break
+            elif player2.energy <= 0 and player1.energy <= 0:
+                time.sleep(1)
+                print(f'Empate! Se viene un nuevo combate!')
+                print('******************************')
+                print('█▀▀ █▀█   ▄▀█ █▀▀ ▄▀█ █ █▄░█ █')
+                print('█▄█ █▄█   █▀█ █▄█ █▀█ █ █░▀█ ▄')
+                print('******************************')
+                time.sleep(2)
+                self.counter += 1
+                break
             i += 1
         
         
--- players.py
+++ players.py
@@ -2,28 +2,48 @@
 
 class Player:
     def __init__(self, n):
+        ''' Función constructora de jugadores para participar en Talanakombat.
+            :params n, string que indica el nombre del jugador
+            :params attacks, diccionario que contiene las combinaciones de teclas que cada jugador utilizará para realizar un ataque, la cantidad de energía que este ataque le quita a su oponente, y el nombre de este ataque.
+            :params energy, int que indica la energía que tiene el jugador al partir.
+            :params direction, string que indica la dirección en la que ataca el jugador. 
+        '''
         self.name = n
         self.attacks = {'combinations': [], 'damages': [], 'names':[]}
         self.energy = 6
         self.direction = ''
 
+
     def sayHi(self):
+        ''' Función que presenta al jugador.
+        '''
         print(f'Hola!, mi nombre es {self.name}.')
 
+
     def attack(self, combo_list):
+        '''Función que procesa las combinaciones de teclas de cada jugador y devuelve el movimiento, golpe o ataque que estas representan.
+            :params combo_list, tupla de movimientos y golpes que cada jugador ejecuta en un turno.
+            :retorna damage, int que indica la energía que le quita a su oponente el ataque ejecutado.
+        '''
+        # Diccionario que permite identificar e interpretar movimientos que no son parte de un ataque.
         loose_moves = {'W': 'hacia arriba', 'S':'hacia abajo', 'A':'a la izquierda', 'D':'a la derecha'}
+       # Diccionario que permite identificar e interpretar golpes que no son parte de una combinación de ataque.
         loose_attacks = {'P': 'puño', 'K':'patada'}
         combo_names = self.attacks['names']
         combo_damage = self.attacks['damages']
+        # Lista que combina la tupla de movimientos y golpes de cada turno
         combination = combo_list[0] + combo_list[1]
-        i = 0
         this_move = f'{self.name}'
         damage = 0
+        # Primero se intenta detectar una combinación de teclas que calce con uno de los ataques del jugador.
         if combination in self.attacks['combinations']:
             index = self.attacks['combinations'].index(combination)
             this_move += f' conecta un {combo_names[index]}'
             damage = combo_damage[index]
+        # Si la combinación de teclas no es un ataque, se separa la primera tecla y se evalúa como un movimiento independiente.
+        # Luego, se vuelve a evaluar si la combinación de teclas restante calza con un ataque del jugador. 
         else:
+            i = 0
             while i in range(0, len(combination)):                       
                 char = list(combination).pop(i)
                 if char in loose_moves:
@@ -46,13 +66,19 @@ class Player:
         print(this_move)
         return damage
 
+
     def update_energy(self, damage):
+        '''Función que actualiza la enegería del jugador después del turno del oponente.
+            :params damage, int que representa el daño que le hace al jugador el ataque del oponente
+            :retorna la energía actualizada del jugador.
+            '''
         self.energy -= damage
         return self.energy
 
+
     def reset_energy(self):
+        '''Función que devuelve la energía al jugador, para un nuevo combate.
+            :retorna la energía actualizada del jugador.
+            '''
         self.energy = 6 
         return self.energy
-
-        
-
